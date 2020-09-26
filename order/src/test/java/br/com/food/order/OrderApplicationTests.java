4591c9429e95faded23bd58bd8cab3173a6d020a
Mateus Nogueira <mateus_nogol@outlook.com>
2020-09-26 17:18:34 -0300

    Adiciona micro servico de order

---
+++ order/src/test/java/br/com/food/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.food.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
